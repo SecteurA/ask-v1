af10a5c204f9173772a39970cd82ba423db593b4
SecteurA <112476252+SecteurA@users.noreply.github.com>
2026-03-05 19:19:55 +0000

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,9 +3,8 @@ import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const { name, company, email, phone, subject, message } = await request.json();
 
   const { error } = await resend.emails.send({
